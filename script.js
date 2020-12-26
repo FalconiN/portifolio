5c3ffa54c88f72c473738c9d347a85e82c8f2206
Natan Falconi <nfalconi65@gmail.com>
2020-12-26 17:37:12 -0300

    finished

--- script.js
+++ script.js
@@ -1,42 +1,45 @@
 const hamburguer = document.querySelector(".hamburguer")
+// const toggleModal = document.querySelector('#toggle-modal')
 
 
 hamburguer.addEventListener("click", function() {
     document.querySelector(".container").classList.toggle("show-menu")
 })
 
-document.querySelector('#qtde').addEventListener("change", atualizarPreco)
-document.querySelector('#js').addEventListener("change", atualizarPreco)
-document.querySelector('#layout-sim').addEventListener("change", atualizarPreco)
-document.querySelector('#layout-nao').addEventListener("change", atualizarPreco)
-document.querySelector('#prazo').addEventListener("change", function(){
-    const prazo = document.querySelector("#prazo").value
-    document.querySelector('label[for="prazo"]').innerHTML = `Prazo: ${prazo} semanas`
-    atualizarPreco()
-})
+// for (var i = 0; i < toggleModal.length; i++) {
+//     console.log(toggleModal[i])
+// }
+
+// document.querySelector('#qtde').addEventListener("change", atualizarPreco)
+// document.querySelector('#js').addEventListener("change", atualizarPreco)
+// document.querySelector('#layout-sim').addEventListener("change", atualizarPreco)
+// document.querySelector('#layout-nao').addEventListener("change", atualizarPreco)
+// document.querySelector('#prazo').addEventListener("change", function(){
+//     const prazo = document.querySelector("#prazo").value
+//     document.querySelector('label[for="prazo"]').innerHTML = `Prazo: ${prazo} semanas`
+//     atualizarPreco()
+// })
 
 
+// function atualizarPreco(){
+//     const qtde = document.querySelector("#qtde").value 
+//     const temjs= document.querySelector("#js").checked
+//     const layoutSim = document.querySelector("#layout-sim").checked
+//     const prazo = document.querySelector("#prazo").value
 
 
-function atualizarPreco(){
-    const qtde = document.querySelector("#qtde").value 
-    const temjs= document.querySelector("#js").checked
-    const layoutSim = document.querySelector("#layout-sim").checked
-    const prazo = document.querySelector("#prazo").value
+//     var preco = qtde * 100
+//     if(temjs) {
+//         preco = preco + (preco * 10/100)
+//     }
 
+//     if(layoutSim){
+//         preco = preco + 500
+//     }
 
-    var preco = qtde * 100
-    if(temjs) {
-        preco = preco + (preco * 10/100)
-    }
+//     var taxaUrgencia = 1 - prazo*0.1;
+//     preco = preco + (preco*taxaUrgencia)
 
-    if(layoutSim){
-        preco = preco + 500
-    }
-
-    var taxaUrgencia = 1 - prazo*0.1;
-    preco = preco + (preco*taxaUrgencia)
-
-    document.querySelector("#preco").innerHTML = `R$ ${preco}`
+//     document.querySelector("#preco").innerHTML = `R$ ${preco}`
     
-}
+// }
